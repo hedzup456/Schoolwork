6a2331793aef4c87e2e5504b0da870aec7335493
Richard Henry <richardhenry602@gmail.com>
2015-09-02 11:52:11 +0100

    Ex3 complete

---
+++ src/JavaBooklet1/Ex3.java
@@ -0,0 +1,15 @@
+package JavaBooklet1;
+
+public class Ex3 {
+	public static void main(String[] args){
+		String firstName = new String();
+		String lastName = new String();
+		firstName = "Richard";
+		lastName = "Henry";
+		
+		String interest = new String();
+		interest = "CS, irsoft and SCUBA diving.";
+		
+		System.out.println("Hello my name is " + firstName + " " + lastName + " and I like " + interest);
+	} // Main
+} // Class
