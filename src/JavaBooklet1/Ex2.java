31e5d2b39beda1bc808601d227aba36b16fe0c61
Richard Henry <richardhenry602@gmail.com>
2015-09-02 11:46:25 +0100

    Ex2 in homework

---
+++ src/JavaBooklet1/Ex2.java
@@ -0,0 +1,13 @@
+package JavaBooklet1;
+
+public class Ex2 {
+	public static void main(String[] args){
+		char ch1, ch2, ch3;
+		ch1 = 'y';
+		ch2 = 'e';
+		ch3 = 's';
+		
+		System.out.println("The letters I stored make the word \"" + ch1+ch2+ch3 + "\"");
+	} // main
+} // class
+
