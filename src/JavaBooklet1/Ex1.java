faac217f9b3507bc42a66f6f834cf2f9901cba0b
Richard Henry <richardhenry602@gmail.com>
2015-09-02 11:39:53 +0100

    After Exercise 1

---
+++ src/JavaBooklet1/Ex1.java
@@ -0,0 +1,18 @@
+package JavaBooklet1;
+
+public class Ex1 {
+	public static void main(String[] args){
+		int num1, num2, num3;
+		num1 =12;
+		num2 = 11;
+		num3 = 24;
+		
+		System.out.println("The three numbers I stored are " + num1 + ", " + num2 +" and " + num3);
+		
+		num1 = 35;
+		num2 = 21;
+		num3 = 72;
+		
+		System.out.println("The three numbers I stored are " + num1 + ", " + num2 +" and " + num3);
+	} // main
+} // class
