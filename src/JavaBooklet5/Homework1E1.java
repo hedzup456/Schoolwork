4dfbe96482864a439ed32b6bd7cce1cc9a84ceea
Richard Henry <richardhenry602@gmail.com>
2016-01-20 11:20:05 +0000

    HW1E1 Done

--- src/JavaBooklet5/Homework1E1.java
+++ src/JavaBooklet5/Homework1E1.java
@@ -3,6 +3,8 @@
  */
 package JavaBooklet5;
 
+import java.util.Scanner;
+
 /**
  * @author Richard
  *
@@ -10,15 +12,35 @@ package JavaBooklet5;
 public class Homework1E1 {
 		public static void main(String[] args)
 	   {
-	      System.out.println("\nCalling simpleMethod() ...");
-	      simpleMethod();	//call method
-	      System.out.println("\nNow back to main method");
-	      simpleMethod();
+	      System.out.println("Sentance Analysis");
+	      System.out.println();
+	      System.out.println("Enter a sentence, then press 'Enter'");
+	      System.out.println();
+	      Scanner sc = new Scanner(System.in);
+	      String sentance = sc.nextLine();
+	      sc.close();
+	      System.out.println();
+	      System.out.println("Your sentance contains " + wordCount(sentance) + " words.");
+	      System.out.println("Your sentance contains " + vowelCount(sentance)+ " vowles.");
 	   } //end main method
-	   public static void simpleMethod()
-	   {
-	      System.out.println("\n simpleMethod displays this message");
-	      System.out.print("\thas no parameters\n\treturns no values");
-	   } //end of simpleMethod
+	   private static int wordCount(String sent){
+		   String[] t = sent.split(" ");
+		   return t.length;
+	   }
+	   private static int vowelCount(String sent){
+		   char[] sentCA = sent.toCharArray();
+		   int count = 0;
+		   for (char c: sentCA){
+			   switch (c){
+			   case 'a':	// fall through
+			   case 'e':	// fall through
+			   case 'i':	// fall through
+			   case 'o':	// fall through
+			   case 'u': count++;
+			   default:	// Do nothing
+			   }
+		   }
+		   return count;
+	   }
 
 }
