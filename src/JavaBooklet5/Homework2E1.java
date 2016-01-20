fee6889a8d984e92c70aecc3c3932594aeff12be
Richard Henry <richardhenry602@gmail.com>
2016-01-20 12:29:30 +0000

    Fixed HW2E1

--- src/JavaBooklet5/Homework2E1.java
+++ src/JavaBooklet5/Homework2E1.java
@@ -29,21 +29,28 @@ public class Homework2E1 {
 		}
 		System.out.println("These numbers are pefect:");
 		for (Object i: perf.toArray()) System.out.print((Integer)i + " ");
+		System.out.println("\n[" + perf.size() + " perfect numbers found, " + perf.size()*100/(double)upperBound + "%]");
+		System.out.println("These numbers are neither perfect or deficient: ");
+		for (Object i: xsve.toArray()) System.out.print((Integer)i + " ");
+		System.out.println("\n[" + xsve.size() + " numbers found, " + xsve.size()*100/(double)upperBound + "%]");
 	}
 	private static boolean isPerfect(int posInt){
 		int sumOfFactors = 0;
-		for (int i = 1; i < (posInt+1)/2; i++){	// No point in checking past halfway
-			if (posInt % i == 0) sumOfFactors += i;
+		for (int i = 1; i < (posInt+2)/2; i++){	// No point in checking past halfway
+			if (posInt % i == 0){
+				sumOfFactors += i;
+			}
+			
+			
 		}
-		System.out.println(posInt + "f: " + sumOfFactors);
 		return sumOfFactors == posInt;
 	}
 	private static boolean isDefic(int posInt){
 		int sumOfFactors = 0;
-		for (int i = 1; i <= (posInt); i++){
+		for (int i = 1; i <= (posInt+2)/2; i++){
 			if (posInt % i == 0) sumOfFactors += i;
 		}
-		return sumOfFactors < posInt;
+		return sumOfFactors > posInt;
 	}
 
 }
