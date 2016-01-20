89b49099a6e6344724a7c2959aeb5474e501e3da
Richard Henry <richardhenry602@gmail.com>
2016-01-20 11:40:30 +0000

    HW1E2 Done

--- src/JavaBooklet5/Homework1E2.java
+++ src/JavaBooklet5/Homework1E2.java
@@ -10,41 +10,25 @@ import java.util.Scanner;
  *
  */
 public class Homework1E2 {
-	   public static void main(String[] args)
-	   {
+	   public static void main(String[] args){
 		   Scanner sc = new Scanner(System.in);
-	      System.out.print("\nfirst integer: ");
-	      int int1 = sc.nextInt();
-	      System.out.print("second integer: ");
-	      int int2 = sc.nextInt();
-	      displaySum(int1, int2);
-
-	      System.out.print("\nfirst decimal: ");
-	      double db1 = sc.nextDouble();
-	      System.out.print("second decimal: ");
-	      double db2 = sc.nextDouble();
-	      displaySum(db1, db2);
-	      
-	      wowOverloading("~");
-	      wowOverloading(1,1);
-	   } //end main method
-	   
-	   public static void displaySum(int num1, int num2)
-	   {
-	      System.out.println("\nInside integer displaySum: ");
-	      System.out.print("\t"+num1+ " + "+num2+ " = " + (num1 +num2));
-	   } //end displaySum (int,int)
-	   public static void displaySum(double n1, double n2)
-	   {
-	      System.out.println("\nInside decimal displaySum: ");
-	      System.out.print("\t"+n1+ " + "+n2+" = " + (n1 +n2));
-	   } //end displaySum (double, double)
-	   
-	   private static void wowOverloading(String text){
-		   System.out.println(text);
+		   System.out.print("Message: ");
+		   String mesg = sc.nextLine();
+		   sc.close();
+		   
+		   System.out.println(swapChars(mesg));
 	   }
-	   private static void wowOverloading(int a, int b){
-		   System.out.println("a^2 + b^2 = c ^2, c = "  + Math.sqrt(a*a+b*b));
+	   private static String swapChars(String pt){
+		   StringBuilder ct = new StringBuilder(pt);
+		   for (int i = 0; i < ct.length(); i += 2){
+			   if (i != ct.length()-1){
+			   char t = ct.charAt(i);
+			   ct.setCharAt(i, ct.charAt(i+1));
+			   ct.setCharAt(i+1, t);
+			   }
+		   }
+		   return ct.toString();
 	   }
+	   
 
 }
